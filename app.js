6ddf478c8d55215baca0fb37b0c1161b5ec0f3f3
abdulrehman998 <armalik0603@gmail.com>
2021-04-03 20:37:15 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,71 @@
+//CHAPTER 17-20
+// Question no 1
+// var Arr=[[],[],[]];
+
+// // Question no 2
+// var Matrix = [["0123"],[1012],[2101]];
+// document.write(Matrix[0] + "<br>" + Matrix[1] + "<br>" + Matrix[2] );
+
+// Question no 3
+// var a = 1;
+// for(a; a<=10; a++)
+// {
+//     document.write(a + "<br>");
+// }
+
+// Question no 4
+// var Number = prompt("Enter a number to show its multiplication");
+// var Length = prompt("Enter a Length of multiplication you want");
+// document.write("Multiplication table of " + Number + "<br>");
+// document.write("Length " + Length + "<br>" + "<br>");
+// for(a=1; a<=Length; a++)
+// {
+
+//      document.write(Number + "x" + a + "=" + (Number*a) + "<br>");
+//  }
+
+// Question no 5
+// var fruits = ["apple","banana","mango","orange","strawbery"];
+//  for (var i=0; i<fruits.length; i++){
+//      document.write(fruits[i] + "<br>");
+//      }
+//      document.write("<br>");
+// document.write("Element at index 0 is " + fruits[0] +"<br>")
+// document.write("Element at index 1 is " + fruits[1] +"<br>")
+// document.write("Element at index 2 is " + fruits[2] +"<br>")
+// document.write("Element at index 3 is " + fruits[3] +"<br>")
+// document.write("Element at index 4 is " + fruits[4] +"<br>")
+
+// Question no 6
+// var counting = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// var reverse_counting = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// var even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+// var odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+// var series = ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"];
+// document.write("Counting:".bold() + "<br>" + "<br>" + counting + "<br>"+ "<br>");
+// document.write("Reverse Counting:".bold() + "<br>" + "<br>" + reverse_counting + "<br>"+ "<br>");
+// document.write("Even:".bold() + "<br>" + "<br>" + even + "<br>"+ "<br>");
+// document.write("Odd:".bold() + "<br>" + "<br>" + odd + "<br>"+ "<br>");
+// document.write("Series:".bold() + "<br>"+ "<br>");
+// for(var i=0; i<series.length; i++)
+// {
+//     document.write( series[i] + ",");
+// }
+
+
+// Question no 8
+// var A = [24, 53, 78, 91, 12]
+// document.write("Array items: " + A + "<br>");
+// document.write("The largest number is " + Math.max(...A));
+
+// Question no 9
+// A = [24, 53, 78, 91, 12]
+// document.write("Array items: " + A + "<br>");
+// document.write("The smallest number is " + Math.min(...A));
+
+// Question no 10
+// var a = 5;
+// for(var b=1; b<=20; b++)
+// {
+//     document.write(a*b + ",");
+// }
